7716dd2a895d9a78d352b59b9075c34c9268150b
IsherfG <51360372+IsherfG@users.noreply.github.com>
2019-07-31 13:34:47 +0630

    feature4

---
+++ Assets/penguinController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class penguinController : MonoBehaviour
+{
+    public Transform[] points;
+    private int num;
+    private float time = 1f;
+
+    void Update()
+    {
+        time -= 1 * Time.deltaTime;
+        num = Random.Range(1,3);
+        if(time <= 0)
+        {
+            transform.position = points[num].position;
+            time = 3.5f;
+        }
+
+    }
+    
+}
